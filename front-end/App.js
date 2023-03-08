8d3f5ca7c78f803444a2ff55f67436fb5dcb7a43
Soli <asoliman96@gmail.com>
2023-03-08 12:33:20 +0100

    checkpoint

---
+++ front-end/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
