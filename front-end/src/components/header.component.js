99ee33a2ba7bde360d667d24e97952b458cbad45
Soli <asoliman96@gmail.com>
2023-03-10 00:59:29 +0100

    refactor: clean

--- front-end/src/components/header.component.js
+++ front-end/src/components/header.component.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Linking } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { HStack, Text, Button, Icon } from "native-base";
+import { HStack, Button, Icon } from "native-base";
 import { Logo } from "./logo.component";
 
 export const Header = () => (
