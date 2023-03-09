bd78d0fe808e97d0cc0e089f9dcdfbca39d38b51
Soli <asoliman96@gmail.com>
2023-03-09 23:06:10 +0100

    feat: logo back to home

--- front-end/src/components/logo.component.js
+++ front-end/src/components/logo.component.js
@@ -1,38 +1,46 @@
 import React from "react";
-import { Box, HStack, Text } from "native-base";
+import { Box, HStack, Text, Pressable } from "native-base";
+import { useNavigation } from "@react-navigation/native";
 
 export const Logo = () => {
   const size = 5;
+  const navigation = useNavigation();
   return (
-    <HStack alignItems={"center"} space={"xs"}>
-      <Box
-        width={size}
-        height={size}
-        borderRadius={"10%"}
-        alignSelf={"center"}
-        bg={{
-          linearGradient: {
-            colors: ["#1982FC", "#316dca"],
-            start: [0, 1],
-            end: [1, 1],
-          },
-        }}
-      />
-      <Text fontWeight={"bold"} fontSize="lg">
-        SongGPT.
-        <Text
-          style={{
-            // Add a linear gradient background to the text
-            background: "linear-gradient(to right, #1982FC, #316dca)",
-            // Clip the background to the text
-            webkitBackgroundClip: "text",
-            // Make the text transparent
-            WebkitTextFillColor: "transparent",
+    <Pressable
+      onPress={() => {
+        navigation.navigate("SongCreate");
+      }}
+    >
+      <HStack alignItems={"center"} space={"xs"}>
+        <Box
+          width={size}
+          height={size}
+          borderRadius={"10%"}
+          alignSelf={"center"}
+          bg={{
+            linearGradient: {
+              colors: ["#1982FC", "#316dca"],
+              start: [0, 1],
+              end: [1, 1],
+            },
           }}
-        >
-          xyz
+        />
+        <Text fontWeight={"bold"} fontSize="lg">
+          SongGPT.
+          <Text
+            style={{
+              // Add a linear gradient background to the text
+              background: "linear-gradient(to right, #1982FC, #316dca)",
+              // Clip the background to the text
+              webkitBackgroundClip: "text",
+              // Make the text transparent
+              WebkitTextFillColor: "transparent",
+            }}
+          >
+            xyz
+          </Text>
         </Text>
-      </Text>
-    </HStack>
+      </HStack>
+    </Pressable>
   );
 };
